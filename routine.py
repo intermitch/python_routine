90cce8b8c6b20fff8e607d49684453d697bf3f3f
MichelCollegeAlma <102301494+MichelCollegeAlma@users.noreply.github.com>
2024-11-15 11:42:06 -0500

    indicator adaptatif

--- routine.py
+++ routine.py
@@ -4,8 +4,10 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import datetime
 import argparse
+import json
+from pathlib import Path
 
-from libs import data
+#from libs import data
 
 # Vérifier si un fichier d'événements a été fourni en argument
 if len(sys.argv) < 2:
@@ -34,8 +36,29 @@ args = parser.parse_args()
 scenario_dir = os.path.join(os.path.dirname(__file__), "scenario")
 events_file = os.path.join(scenario_dir, sys.argv[1] + ".json")
 
+
+# Charger les événements, le titre, l'heure de début et de fin depuis le fichier JSON
+def load_data(file_path):
+    # Chemin vers la racine du projet
+    project_root = Path(__file__).resolve().parents[0]
+    with open(file_path, "r", encoding="utf-8") as f:  # Utiliser l'encodage UTF-8
+        data = json.load(f)
+    # Ajouter le chemin complet pour chaque icône
+    events_data = data.get("events", [])
+    for event in events_data:
+        event["icon_path"] = os.path.join(project_root, "images", event["icon"])
+    # Ajouter le chemin complet pour chaque icône
+    indicators_data = data.get("indicators", [])
+    for indicator in indicators_data:
+        indicator["icon_path"] = os.path.join(project_root, "images", indicator["icon"])
+    # Extraire le titre, l'heure de début et de fin
+    title = data.get("title", "Titre")
+    start_hour = data.get("start_hour", "00:00")
+    end_hour = data.get("end_hour", "23:59")
+    return title, start_hour, end_hour, events_data, indicators_data
+
 # Charger les données
-title, start_hour, end_hour, events, indicators = data.load_data(events_file)
+title, start_hour, end_hour, events, indicators = load_data(events_file)
 
 # Créer start_time et end_time avec la date actuelle
 today = datetime.datetime.now().date()
