ee8e930e28d9499498b3b51adb9b89f08c4e227c
MichelCollegeAlma <102301494+MichelCollegeAlma@users.noreply.github.com>
2024-12-04 08:59:00 -0500

    Ajout audio a main

--- routine.py
+++ routine.py
@@ -7,15 +7,3 @@ if __name__ == "__main__":
 
     app = TimelineApp(events_file, fullscreen=fullscreen)
     app.run()
-
-
-#test audio
-
-#from pygame import mixer
-#mixer.init()
-#mixer.music.load("sounds/button.mp3")  # Remplacez par le chemin exact
-#mixer.music.play()
-#
-## Ajoutez une pause pour écouter le son
-#import time
-#time.sleep(5)  # Attend 5 secondes pour laisser le son jouer
